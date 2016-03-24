6674172e5abf06ca16e4d05d35ba460fbf527caf
thibaut-duguet <thibaut.duguet@gmail.com>
2016-03-24 15:17:59 +0100

    test transfer data

--- module_mongolab.py
+++ module_mongolab.py
@@ -19,6 +19,21 @@ def alreadyInCollection(text):
     else:
         return False
 
+def getCommunityInfo(text):
+    results_cursor = db.twitter_community.find({'query': text})
+    for document in results_cursor:
+        return document
+
+def communityInfo(text):
+    raw_result = getCommunityInfo(text)
+    communities = {}
+    for i in range(1,4):
+        communities[i-1] = i-1
+        communities['size'] = len(raw_result['communities'][i-1]['screen_names'])
+        communities['words'] = raw_result['communities'][i-1]['words']
+    return communities
+
+
 #Download query result from Mongolab (replace collection by collection name before running)
 #def downloadFromMongolab():
 #    results = db.collection.find()
