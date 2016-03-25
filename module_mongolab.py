cd2dbbb11c23d92bcf4947418105675cc8cc0e60
thibaut-duguet <thibaut.duguet@gmail.com>
2016-03-25 11:06:14 +0100

    js code added

--- module_mongolab.py
+++ module_mongolab.py
@@ -41,7 +41,7 @@ def communitySize(text, i):
 def communitySizePercent(text, i):
     comSize = communitySize(text, i)
     totalSize = communitySize(text, 0) + communitySize(text, 1) + communitySize(text, 2) + communitySize(text, 3)
-    return comSize/float(totalSize) * 100
+    return format(comSize/float(totalSize) * 100, '.2f')
 
 #Download query result from Mongolab (replace collection by collection name before running)
 #def downloadFromMongolab():
